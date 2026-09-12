a561b999cd92c7466f27fe193d9f7b6e64d8d4cf
Skokoulin <kokoulinstas@gmail.com>
2026-09-12 22:54:19 +0300

    Обновление сборки 12.09.2026 22:54: Журнал — даты, просмотры, подпись, обложки

--- assets/FavoritesPage-fk2CM3T2.js
+++ assets/FavoritesPage-fk2CM3T2.js
@@ -1,4 +1,4 @@
-import{c as _,W as w,X as D,r as p,U as y,j as e,a8 as k,a as g,M as C,n as N,t as j,G as P,a9 as S,aa as v,ab as M,ac as z,Y as E,u as $,$ as T,a1 as I,p as B,a6 as F,a2 as L}from"./index-DIOzjGfW.js";import{f as A,M as G,c as O,e as H}from"./share-BzDgYTbG.js";/**
+import{c as _,W as w,X as D,r as p,U as y,j as e,a8 as k,a as g,M as C,n as N,t as j,G as P,a9 as S,aa as v,ab as M,ac as z,Y as E,u as $,$ as T,a1 as I,p as B,a6 as F,a2 as L}from"./index-C4mvSPim.js";import{f as A,M as G,c as O,e as H}from"./share-wqe_g8Xk.js";/**
  * @license lucide-react v1.28.0 - ISC
  *
  * This source code is licensed under the ISC license.
