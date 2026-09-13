26979e430986d82255a68c72ee57426886b95e61
Skokoulin <kokoulinstas@gmail.com>
2026-09-14 01:03:11 +0300

    Обновление сборки 14.09.2026 01:03: газета + кабинет без цикла

--- assets/share-DwF-p3iX.js
+++ assets/share-DwF-p3iX.js
@@ -1,4 +1,4 @@
-import{c as Zp,ad as Gp,ae as Hp,r as Ri,g as Xp,h as Wp,j as We,af as iu,ag as Kp,a6 as Jp,ah as Yp}from"./index-D3T0Pvja.js";/**
+import{c as Zp,ad as Gp,ae as Hp,r as Ri,g as Xp,h as Wp,j as We,af as iu,ag as Kp,a6 as Jp,ah as Yp}from"./index-c3-MLkvl.js";/**
  * @license lucide-react v1.28.0 - ISC
  *
  * This source code is licensed under the ISC license.
